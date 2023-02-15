22ce664839396737179174dc11dc167f8ad7a936
sureshlunavath <71497381+sureshlunavath@users.noreply.github.com>
2023-02-15 18:27:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const path = require('path')
 // mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist/mobileApp/')));
+app.use(express.static(path.join(__dirname, '/dist/mobileApp/')));
 
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/mobileApp/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/mobileApp/index.html'));
 });
-
+//remove / before dist to deploy
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
