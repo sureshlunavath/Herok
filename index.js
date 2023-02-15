e9f8ed6975b7ba9996aef4748c42a3ab948753ba
sureshlunavath <sureshlunavath7@gmail.com>
2023-02-15 17:40:05 +0530

    changed index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-
+const path = require('path')
 /* 
     Incase you are using mongodb atlas database uncomment below line
     and replace "mongoAtlasUri" with your mongodb atlas uri.
