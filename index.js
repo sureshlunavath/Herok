9f340732c92554edfc7d2aaa404c664256fb7c72
sureshlunavath <sureshlunavath7@gmail.com>
2023-02-15 17:35:30 +0530

    addded files

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const port = process.env.PORT || 3000
 // mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, 'dist/')));
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 
